bf460f0ef240716a4bd6191eaacf7aca233b3ed0
Vijay kumar Tumati <vtumati@codeaurora.org>
2014-06-17 21:22:58 +0530

    Camera: Fix longshot issue in zsl and non zsl modes
    There was a mistake in number of buffers
    calculation for long shot mode and
    this change fixes it.
    
    Change-Id: I4b1e642e0a80c44393115693a2e85de008d3906f

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -1592,9 +1592,11 @@ uint8_t QCamera2HardwareInterface::getBufNumRequired(cam_stream_type_t stream_ty
     case CAM_STREAM_TYPE_OFFLINE_PROC:
         {
             bufferCnt = minCaptureBuffers;
-            if ((mLongshotEnabled) &&
-                    ( bufferCnt > CAMERA_LONGSHOT_STAGES ) ) {
+            if (mLongshotEnabled) {
                 bufferCnt = CAMERA_LONGSHOT_STAGES;
+            }
+            if (bufferCnt > maxStreamBuf) {
+                bufferCnt = maxStreamBuf;
             }
         }
         break;
