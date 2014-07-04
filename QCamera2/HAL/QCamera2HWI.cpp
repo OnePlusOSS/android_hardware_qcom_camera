a628b74f9e56e652bfb13b95b78071581261a440
Abhishek Pant <apant@codeaurora.org>
2014-07-04 15:56:30 +0530

    QCamera2: Support to enable/disable HDR 1x frame
    Adding support for enabling/disabling HDR 1x frame
    from UI instead of it being enabled by default.
    
    Change-Id: Ibfc3c32628028224a0ef692333e6d573089b3e14
    CRs-fixed: 686647

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -6599,7 +6599,7 @@ bool QCamera2HardwareInterface::isRegularCapture()
     if (numOfSnapshotsExpected() == 1 &&
         !isLongshotEnabled() &&
         !mParameters.getRecordingHintValue() &&
-        !isZSLMode()) {
+        !isZSLMode() && !(mParameters.isHDREnabled())) {
             ret = true;
     }
     return ret;
