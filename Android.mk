7b39728626907c025ab5f6b9a89b35cc1f424678
jiachunxu <leo.jia@oneplus.net>
2017-02-07 10:54:30 +0800

    use prebuild camera hal
    Change-Id: I2e208fac1219359196eab553222170d6ac5f821f

--- Android.mk
+++ Android.mk
@@ -18,10 +18,13 @@ MM_V4L2_DRIVER_LIST += msm8953
 MM_V4L2_DRIVER_LIST += msmcobalt
 MM_V4L2_DRIVER_LIST += msmfalcon
 
+#CAMERA_BUILD_HAL := true
+ifdef CAMERA_BUILD_HAL
 ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
   ifneq ($(strip $(USE_CAMERA_STUB)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
       include $(call all-subdir-makefiles)
     endif
   endif
+endif
 endif
