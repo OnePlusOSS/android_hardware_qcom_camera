sonyxperiadev/camera

QCamera2: HAL3: Add support for Auto HDR metadata
-Update available scene modes
-Expose scene detection for HDR with confidence values

Change-Id: I5d9dc9ded9e06c3a7e36f2cfcffa498e2dbd7a49

QCamera2/HAL3/QCamera3VendorTags.cpp
@@ -65,7 +65,8 @@ enum qcamera3_ext_tags qcamera3_ext3_section_bounds[QCAMERA3_SECTIONS_END -
         QCAMERA3_INSTANT_AEC_END,
         QCAMERA3_SHARPNESS_END,
         QCAMERA3_HISTOGRAM_END,
-        QCAMERA3_BINNING_CORRECTION_END
+        QCAMERA3_BINNING_CORRECTION_END,
+        QCAMERA3_STATS_END
 };
 
 typedef struct vendor_tag_info {
@@ -97,7 +98,8 @@ const char *qcamera3_ext_section_names[QCAMERA3_SECTIONS_END -
     "org.codeaurora.qcamera3.instant_aec",
     "org.codeaurora.qcamera3.sharpness",
     "org.codeaurora.qcamera3.histogram",
-    "org.codeaurora.qcamera3.binning_correction"
+    "org.codeaurora.qcamera3.binning_correction",
+    "org.codeaurora.qcamera3.stats"
 };
 
 vendor_tag_info_t qcamera3_privatedata[QCAMERA3_PRIVATEDATA_END - QCAMERA3_PRIVATEDATA_START] = {
@@ -231,11 +233,16 @@ vendor_tag_info_t qcamera3_histogram[QCAMERA3_HISTOGRAM_END -
     { "stats", TYPE_INT32 }
 };
 
-vendor_tag_info_t
-        qcamera3_binning_correction[QCAMERA3_BINNING_CORRECTION_END -
+vendor_tag_info_t qcamera3_binning_correction[QCAMERA3_BINNING_CORRECTION_END -
         QCAMERA3_BINNING_CORRECTION_START] = {
     { "binning_correction_mode", TYPE_INT32 },
     { "binning_correction_available_modes",   TYPE_INT32 }
+};
+
+vendor_tag_info_t qcamera3_stats[QCAMERA3_STATS_END -
+        QCAMERA3_STATS_START] = {
+    { "is_hdr_scene", TYPE_BYTE },
+    { "is_hdr_scene_confidence", TYPE_FLOAT }
 };
 
 vendor_tag_info_t *qcamera3_tag_info[QCAMERA3_SECTIONS_END -
@@ -262,7 +269,8 @@ vendor_tag_info_t *qcamera3_tag_info[QCAMERA3_SECTIONS_END -
     qcamera3_instant_aec,
     qcamera3_sharpness,
     qcamera3_histogram,
-    qcamera3_binning_correction
+    qcamera3_binning_correction,
+    qcamera3_stats
 };
 
 uint32_t qcamera3_all_tags[] = {
@@ -350,10 +358,14 @@ uint32_t qcamera3_all_tags[] = {
     (uint32_t)QCAMERA3_HISTOGRAM_BUCKETS,
     (uint32_t)QCAMERA3_HISTOGRAM_MAX_COUNT,
     (uint32_t)QCAMERA3_HISTOGRAM_STATS,
+
     // QCAMERA3_BINNING_CORRECTION_END
     (uint32_t)QCAMERA3_BINNING_CORRECTION_MODE,
-    (uint32_t)QCAMERA3_AVAILABLE_BINNING_CORRECTION_MODES
+    (uint32_t)QCAMERA3_AVAILABLE_BINNING_CORRECTION_MODES,
 
+    // QCAMERA3_STATS
+    (uint32_t)QCAMERA3_STATS_IS_HDR_SCENE,
+    (uint32_t)QCAMERA3_STATS_IS_HDR_SCENE_CONFIDENCE
 };
 
 const vendor_tag_ops_t* QCamera3VendorTags::Ops = NULL;

OnePlusOSS/android_hardware_qcom_camera

QCamera2/HAL3/QCamera3VendorTags.cpp
@@ -58,7 +58,8 @@ enum qcamera3_ext_tags qcamera3_ext3_section_bounds[QCAMERA3_SECTIONS_END -
         QCAMERA3_DUALCAM_CALIB_META_DATA_END,
         QCAMERA3_HAL_PRIVATEDATA_END,
         QCAMERA3_JPEG_ENCODE_CROP_END,
-        QCAMERA3_SHARPNESS_END
+        QCAMERA3_SHARPNESS_END,
+        QCAMERA3_STATS_END
 };
 
 typedef struct vendor_tag_info {
@@ -83,7 +84,8 @@ const char *qcamera3_ext_section_names[QCAMERA3_SECTIONS_END -
     "org.codeaurora.qcamera3.dualcam_calib_meta_data",
     "org.codeaurora.qcamera3.hal_private_data",
     "org.codeaurora.qcamera3.jpeg_encode_crop",
-    "org.codeaurora.qcamera3.sharpness"
+    "org.codeaurora.qcamera3.sharpness",
+    "org.codeaurora.qcamera3.stats"
 };
 
 vendor_tag_info_t qcamera3_privatedata[QCAMERA3_PRIVATEDATA_END - QCAMERA3_PRIVATEDATA_START] = {
@@ -180,6 +182,13 @@ vendor_tag_info_t qcamera3_sharpness[QCAMERA3_SHARPNESS_END -
     {"range", TYPE_INT32 }
 };
 
+
+vendor_tag_info_t qcamera3_stats[QCAMERA3_STATS_END -
+        QCAMERA3_STATS_START] = {
+    { "is_hdr_scene", TYPE_BYTE },
+    { "is_hdr_scene_confidence", TYPE_FLOAT }
+};
+
 vendor_tag_info_t *qcamera3_tag_info[QCAMERA3_SECTIONS_END -
         VENDOR_SECTION] = {
     qcamera3_privatedata,
@@ -197,7 +206,8 @@ vendor_tag_info_t *qcamera3_tag_info[QCAMERA3_SECTIONS_END -
     qcamera3_dualcam_calib_meta_data,
     qcamera3_hal_privatedata,
     qcamera3_jpep_encode_crop,
-    qcamera3_sharpness
+    qcamera3_sharpness,
+    qcamera3_stats
 };
 
 uint32_t qcamera3_all_tags[] = {
@@ -260,7 +270,11 @@ uint32_t qcamera3_all_tags[] = {
 
     //QCAMERA3_SHARPNESS
     (uint32_t)QCAMERA3_SHARPNESS_STRENGTH,
-    (uint32_t)QCAMERA3_SHARPNESS_RANGE
+    (uint32_t)QCAMERA3_SHARPNESS_RANGE,
+
+    // QCAMERA3_STATS
+    (uint32_t)QCAMERA3_STATS_IS_HDR_SCENE,
+    (uint32_t)QCAMERA3_STATS_IS_HDR_SCENE_CONFIDENCE
 };
 
 const vendor_tag_ops_t* QCamera3VendorTags::Ops = NULL;