76e36efb74888d3e36ffc7b554d0675dab8b554d
Naveen Kumar <kumarn@codeaurora.org>
2017-12-08 08:50:13 +0530

    Revert "QCamera2:HAL :Changes to get the vsync event and presentation timestamp".
    This reverts commit 38f90d257451622e53c84b191d9a49b7fa062865.
    
    Change-Id: Id86a6d8ef0530b8b8d2d2eafebfd412679f35412

--- QCamera2/HAL/QCamera2HWI.h
+++ QCamera2/HAL/QCamera2HWI.h
@@ -209,9 +209,6 @@ private:
     QCameraCmdThread mProcTh;
     bool             mActive;
 };
-
-class QCameraDisplay;
-
 class QCamera2HardwareInterface : public QCameraAllocator,
         public QCameraThermalCallback, public QCameraAdjustFPS
 {
@@ -738,9 +735,6 @@ private:
         CMD_DEF_PARAM_ALLOC,
         CMD_DEF_PARAM_INIT,
         CMD_DEF_GENERIC,
-#ifdef USE_DISPLAY_SERVICE
-        CMD_DEF_DISPLAY_INIT,
-#endif //USE_DISPLAY_SERVICE
         CMD_DEF_MAX
     };
 
@@ -848,7 +842,7 @@ private:
     uint32_t mSurfaceStridePadding;
 
     //QCamera Display Object
-    QCameraDisplay* mCameraDisplay;
+    //QCameraDisplay mCameraDisplay;
 
     bool m_bNeedRestart;
     Mutex mMapLock;
